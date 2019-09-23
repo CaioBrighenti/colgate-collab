201f04b9d62b43249f7d2f41738242e0535ca56d
caiobrighenti <caio.brig@gmail.com>
2019-09-23 10:10:49 -0400

    implement data upload and var selection

--- tutorial_app/app.R
+++ tutorial_app/app.R
@@ -10,64 +10,67 @@ ui <- fluidPage(
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
+      h3('Data Upload'),
       # Input: File browse to load data
       fileInput(inputId = "dat_file",
                 label = "File input"
       ),
-      
       # Input: Drop down to select y var
       selectInput(inputId = "y_var",
                   label = "Continuous variable",
                   choices = c("y1","y2")),
-      
       # Input: Drop down to select x var
       selectInput(inputId = "x_var",
                   label = "Categorical variable",
                   choices = c("x1","x2"))
-      
-      
     ),
     # Main panel for displaying outputs ----
     mainPanel(
-      
       # Output: Histogram ----
       plotOutput(outputId = "violinPlot"),
-      fluidRow(column(4, verbatimTextOutput("value")))
-      
-    )
+      #fluidRow(column(4, verbatimTextOutput("value"))),
+      hr()
+    ),
   )
 )
 
 # Define server logic required to draw a histogram ----
 server <- function(input, output, session) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
+  ## creates a reactive violin plot from user uploaded data
   output$violinPlot <- renderPlot({
+    ## ensure data loaded, else stop
     req(v$dat)
+    ## pull out x and y 
     x<-v$dat[,which(colnames(v$dat) == input$x_var)]
     y<-v$dat[,which(colnames(v$dat) == input$y_var)]
+    ## create data frame for plotting
+    dat<-data.frame(x,y)
+    ## plot data
+    ggplot(dat, aes(x=x,y=y))+
+      geom_violin(aes(fill = dat$x), color='black', alpha=0.5)+
+      geom_boxplot(width=0.1) +
+      theme_minimal()
     
   })
   
+  ## init reactive values
   v <- reactiveValues()
   
+  ## test output
   output$value <- renderPrint({
-    str(v$dat)
+    #str(v$dat)
   })
   
+  
+  ## loads data into reactive input and updates necessary panels
+  ## runs when user uploads a new file
   observeEvent(input$dat_file,{
+    ## get file path
     url = input$dat_file$datapath
+    ## load using appropriate method
     tryCatch(
       {input_data <- switch(file_ext(url),
                             "csv"=read.csv(url),
@@ -80,19 +83,21 @@ server <- function(input, output, session) {
         return()
       }
     )
+    ## ensure data load
     req(input_data)
-    v$dat <- as_tibble(input_data)
     
+    ## update drop downs to use variable names
+    factor_idx <- which(sapply(input_data, class) %in% c('factor', 'logical'))
     updateSelectInput(session, "y_var",
-                      label = paste("Select input label", length(colnames(v$dat))),
-                      choices = colnames(v$dat),
-                      selected = tail(colnames(v$dat), 1)
+                      choices = colnames(input_data[-factor_idx]),
+                      selected = tail(colnames(input_data[-factor_idx]), 1)
     )
     updateSelectInput(session, "x_var",
-                      label = paste("Select input label", length(colnames(v$dat))),
-                      choices = colnames(v$dat),
-                      selected = tail(colnames(v$dat), 1)
+                      choices = colnames(input_data[factor_idx]),
+                      selected = tail(colnames(input_data[factor_idx]), 1)
     )
+    ## put data into reactive value
+    v$dat <- input_data
   })  
   
 }
